c0071f51a4e7334e91f21d200b106a3442b7bde2
andrequeiroz <andrenaq@gmail.com>
2022-12-16 20:17:00 -0500

    day 06

---
+++ day06.py
@@ -0,0 +1,19 @@
+# https://adventofcode.com/2022/day/3
+import csv
+
+message = ""
+with open('adventofcode_2022/input_files/day06_input.csv') as csvfile:
+    csvReader = csv.reader(csvfile, delimiter=',')
+    for row in csvReader:
+        message = row[0]
+
+
+def get_beggining(msg, size):
+    for i in range(size, len(msg) - 1):
+        if len(set(msg[i-size:i])) == len(msg[i-size:i]):
+            return i
+    return 0
+
+
+print("Part 01: ", get_beggining(message, 4))
+print("Part 02: ", get_beggining(message, 14))
